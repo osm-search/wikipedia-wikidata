087a9ace2a54e5ec7019d22d8314443edbb232f8
marc tobias <mtmail@gmx.net>
2023-05-04 02:56:09 +0200

    latest_available_data: we cant rely on index.json

--- steps/latest_available_data.sh
+++ steps/latest_available_data.sh
@@ -7,10 +7,118 @@
 #
 
 debug() {
-	# Comment out the following line to print debug information
-	# echo "$@" 1>&2;
-	echo -n ''
+    # Comment out the following line to print debug information
+    # echo "$@" 1>&2;
+    echo -n ''
 }
+
+
+DATE=''
+
+# Sets $DATE to first of the month (YYYYMMDD). If given a parameter then
+# it substracts number of months
+set_date_to_first_of_month() {
+    MINUS_NUM_MONTHS=${1:-0}
+
+    if [[ "$(uname)" == "Darwin" ]]; then
+        DATE=$(date -v -${MINUS_NUM_MONTHS}m +%Y%m01) 
+    else
+        DATE=$(date --date="-$MINUS_NUM_MONTHS month" +%Y%m01) 
+    fi
+}
+
+
+check_all_files_ready() {
+    CHECK_DATE=$1
+    debug "check_all_files_ready for $CHECK_DATE"
+
+    # The complete dump for wikidata for example can take several weeks (metahistory7zdump
+    # file ready after 15 days).
+    #
+    # The dumpruninfo.json files have this format:
+    # {
+    #   "jobs": {
+    #     "imagetable": {
+    #       "status": "done",
+    #       "updated": "2023-02-01 08:27:30"
+    #     },
+    #     "imagelinkstable": {
+    #       "status": "done",
+    #       "updated": "2023-02-01 09:18:03"
+    #     },
+    #     "geotagstable": {
+    #       "status": "done",
+    #       "updated": "2023-02-01 10:01:50"
+    #     },
+    #     [...]
+    #
+
+    ANY_FILE_MISSING=0
+
+
+    ##
+    ## 1. Chinese (ZH) Wikipedia
+    ## usually the last to be dumped
+    ##
+    # from wikipedia_download.sh
+    WIKIPEDIA_REQUIRED_FILES="page pagelinks langlinks redirect"
+    DUMP_RUN_INFO_URL="https://mirror.clarkson.edu/wikimedia/zhwiki/$CHECK_DATE/dumpruninfo.json"
+    debug $DUMP_RUN_INFO_URL
+    DUMP_RUN_INFO=$(curl -s --fail "$DUMP_RUN_INFO_URL")
+
+    if [[ $? != 0 ]]; then
+        debug "fetching from URL $DUMP_RUN_INFO_URL failed"
+        return 1
+    fi
+
+
+    for FN in $WIKIPEDIA_REQUIRED_FILES; do
+        TABLENAME=${FN//_/}table # redirect => redirecttable
+        debug "checking status for table $TABLENAME"
+
+        STATUS=$(echo "$DUMP_RUN_INFO" | TABLE=$TABLENAME jq -r '.jobs[env.TABLE].status')
+        debug "  status: $STATUS"
+
+        if [ "$STATUS" != "done" ]; then
+            debug "$TABLENAME not ready yet"
+            ANY_FILE_MISSING=1
+        fi
+    done
+
+
+
+    ##
+    ## 2. Wikidata
+    ##
+    # from wikidata_download.sh
+    WIKIDATA_REQUIRED_FILES="geo_tags page wb_items_per_site"
+
+    DUMP_RUN_INFO_URL="https://mirror.clarkson.edu/wikimedia/wikidatawiki/$CHECK_DATE/dumpruninfo.json"
+    debug $DUMP_RUN_INFO_URL
+    DUMP_RUN_INFO=$(curl -s --fail "$DUMP_RUN_INFO_URL")
+
+    if [[ $? != 0 ]]; then
+        debug "fetching from URL $DUMP_RUN_INFO_URL failed"
+        return 1
+    fi
+
+    for FN in $WIKIDATA_REQUIRED_FILES; do
+        TABLENAME=${FN//_/}table # wb_items_per_site => wbitemspersitetable
+        debug "checking status for table $TABLENAME"
+
+        STATUS=$(echo "$DUMP_RUN_INFO" | TABLE=$TABLENAME jq -r '.jobs[env.TABLE].status')
+        debug "  status: $STATUS"
+
+        if [ "$STATUS" != "done" ]; then
+            debug "$TABLENAME not ready yet"
+            ANY_FILE_MISSING=1
+        fi
+    done
+
+    return $ANY_FILE_MISSING
+}
+
+
 
 #
 # Usually you might try to get a list of dates from
@@ -19,140 +127,21 @@ debug() {
 #
 # We want to avoid parsing HTML.
 #
-# Instead we look at index.json which is 3 megabyte and contains almost too much
-# information.
-#
-# {
-#   "status": "done",
-#   "updated": "2023-02-01 09:58:04",
-#   "files": {
-#     "enwiki-20230201-page.sql.gz": {
-#       "size": 2068890771,
-#       "url": "/enwiki/20230201/enwiki-20230201-page.sql.gz",
-#       "md5": "b8f692170b3d9ca11157d1def489fbce",
-#       "sha1": "7915d4d5d8baa8f40d49c93786cd77b9b9edd269"
-#     }
-#   }
-# }
-# 
-# We look for enwiki because english language is the largest database and takes the longest
-# to dump. We then assume all other languages are also ready.
-#
-#
-INDEX_JSON_URL="https://mirror.clarkson.edu/wikimedia/index.json"
-debug "$INDEX_JSON_URL"
-INDEX_JSON=$(curl -s "$INDEX_JSON_URL" | jq '.wikis.enwiki.jobs.pagetable')
-if [[ "$INDEX_JSON" = "" ]]; then
-	debug "fetching from URL $INDEX_JSON_URL failed"
-	exit 1
-fi
-
-LATEST_FILENAME=$(echo $INDEX_JSON | jq -r '.files | keys[-1]')
-debug "LATEST_FILENAME: $LATEST_FILENAME"
-# enwiki-20230201-page.sql.gz
-
-# Split into array, use second item
-PARTS=(${LATEST_FILENAME//-/ })
-LATEST_DATE=${PARTS[1]}
-debug "LATEST_DATE: $LATEST_DATE"
-
-if [[ "$LATEST_DATE" = "" ]]; then
-	debug "No date found"
-	exit 1
-fi
-
-
-
-#
-# Now double-check all files we're interested are ready. The complete dump for wikidata for
-# example can take severla weeks (metahistory7zdump file ready after 15 days).
-#
-# The dumpruninfo.json files have this format:
-# {
-#   "jobs": {
-#     "imagetable": {
-#       "status": "done",
-#       "updated": "2023-02-01 08:27:30"
-#     },
-#     "imagelinkstable": {
-#       "status": "done",
-#       "updated": "2023-02-01 09:18:03"
-#     },
-#     "geotagstable": {
-#       "status": "done",
-#       "updated": "2023-02-01 10:01:50"
-#     },
-#     [...]
+# Previous version of this script then looked at index.json
+# (https://mirror.clarkson.edu/wikimedia/index.json) but the file is written at beginning
+# of the export so first of month it would list files that don't exist yet.
 #
 
-ANY_FILE_MISSING=0
+for MINUS_NUM_MONTHS in 0 1 2 3; do
+    set_date_to_first_of_month $MINUS_NUM_MONTHS
+    check_all_files_ready $DATE
+    # echo $?
+    # echo $RES
 
-
-##
-## 1. English Wikipedia
-##
-# from wikipedia_download.sh
-WIKIPEDIA_REQUIRED_FILES="page pagelinks langlinks redirect"
-DUMP_RUN_INFO_URL="https://mirror.clarkson.edu/wikimedia/enwiki/$LATEST_DATE/dumpruninfo.json"
-debug $DUMP_RUN_INFO_URL
-DUMP_RUN_INFO=$(curl -s "$DUMP_RUN_INFO_URL")
-
-if [[ "$DUMP_RUN_INFO" = "" ]]; then
-	debug "fetching from URL $DUMP_RUN_INFO_URL failed"
-	exit 1
-fi
-
-
-for FN in $WIKIPEDIA_REQUIRED_FILES; do
-	TABLENAME=${FN//_/}table # redirect => redirecttable
-	debug "checking status for table $TABLENAME"
-
-	STATUS=$(echo $DUMP_RUN_INFO | TABLE=$TABLENAME jq -r '.jobs[env.TABLE].status')
-	debug "  status: $STATUS"
-
-	if [[ $STATUS -ne 'done' ]]; then
-		debug "$TABLENAME not ready yet"
-		$ANY_FILE_MISSING=1
-	fi
+    if [ $? == 0 ]; then
+        echo "$DATE"
+        exit 0
+    fi
 done
 
-
-
-##
-## 2. Wikidata
-##
-# from wikidata_download.sh
-WIKIDATA_REQUIRED_FILES="geo_tags page wb_items_per_site"
-
-DUMP_RUN_INFO_URL="https://mirror.clarkson.edu/wikimedia/enwiki/$LATEST_DATE/dumpruninfo.json"
-debug $DUMP_RUN_INFO_URL
-DUMP_RUN_INFO=$(curl -s "$DUMP_RUN_INFO_URL")
-
-if [[ "$DUMP_RUN_INFO" = "" ]]; then
-	debug "fetching from URL $DUMP_RUN_INFO_URL failed"
-	exit 1
-fi
-
-for FN in $WIKIDATA_REQUIRED_FILES; do
-	TABLENAME=${FN//_/}table # wb_items_per_site => wbitemspersitetable
-	debug "checking status for table $TABLENAME"
-
-	STATUS=$(echo $DUMP_RUN_INFO | TABLE=$TABLENAME jq -r '.jobs[env.TABLE].status')
-	debug "  status: $STATUS"
-
-	if [[ $STATUS -ne 'done' ]]; then
-		debug "$TABLENAME not ready yet"
-		$ANY_FILE_MISSING=1
-	fi
-done
-
-
-##
-## Finally print the YYYYMMDD date
-##
-debug "ANY_FILE_MISSING: $ANY_FILE_MISSING"
-if [[ $ANY_FILE_MISSING = 0 ]]; then
-	echo $LATEST_DATE
-fi
-
-exit 0
+exit 1
