75c7dc5c81d7b781592fc35c8c53c5a829f5a718
Marc Tobias <mtmail@gmx.net>
2023-07-18 19:09:41 +0200

    update tests

--- tests/run.sh
+++ tests/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cat tests/round_coordinates.test1.txt | bin/round_coordinates.py > out.txt
-diff --brief out.txt tests/round_coordinates.test1expected.txt || exit 1
+cat tests/filter_wikidata_geo_tags.test1.txt | bin/filter_wikidata_geo_tags.py > out.txt
+diff --brief out.txt tests/filter_wikidata_geo_tags.test1expected.txt || exit 1
