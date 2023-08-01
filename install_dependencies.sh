7c7e4deebc0160b852eb93e6bce89675f917ee05
Marc Tobias <mtmail@gmx.net>
2023-08-01 14:15:04 +0200

    assume postgresql already installed

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -7,25 +7,6 @@
 sudo apt-get install -y postgresql-14
 sudo -u postgres createuser -s $USER
 
-# The database can be 100GB or more. If you want to create it on a separate
-# drive you can try:
-#
-# sudo -u postgres psql -c 'SELECT * FROM pg_tablespace;'
-# # oid  |  spcname   | spcowner | spcacl | spcoptions
-# #------+------------+----------+--------+------------
-# # 1663 | pg_default |       10 |        |
-# # 1664 | pg_global  |       10 |        |
-#
-# EXTRASPACE_PATH=/mnt/HC_Volume_21300566/postgres-data
-# sudo mkdir -p $EXTRASPACE_PATH
-# sudo chown postgres $EXTRASPACE_PATH
-# sudo chgrp postgres $EXTRASPACE_PATH
-#
-# sudo -u postgres psql -c "CREATE TABLESPACE extraspace LOCATION '$EXTRASPACE_PATH';"
-# sudo -u postgres psql -c 'SELECT * FROM pg_tablespace;'
-
-
-
 sudo apt-get install -y wget coreutils nodejs jq moreutils pigz
 sudo apt-get install -y python3-dev python3-pip python3-setuptools build-essential
 
